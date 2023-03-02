71d2b79a0e8429dd61fecf536f5f1098b7896c7a
margarita <mitina.mi@phystech.edu>
2023-03-02 21:35:24 +0300

    add headers

--- src/progonka.cpp
+++ src/progonka.cpp
@@ -1,33 +1,6 @@
 #include<vector>
-
-
-class matrix{
-    public:
-        matrix(int m){
-            a.reserve(n-1);
-            b.reserve(n);
-            c.reserve(n-1);
-            n = m;
-        };
-
-        matrix(int m, std::vector<double> a, std::vector<double> b, std::vector<double> c){
-            this->a = a;
-            this->b = b;
-            this->c = c;
-            n = m;
-        };
-
-        int get_n(){return n;};
-        double get_a(int i){return a[i];};
-        double get_b(int i){return b[i];};
-        double get_c(int i){return c[i];};
-
-    private:
-        int n;
-        std::vector<double> a;
-        std::vector<double> b;
-        std::vector<double> c;
-};
+#include"progonka.h"
+#include"tri_diag_matrix.h"
 
 std::vector<double> progonka(matrix* M, std::vector<double> d){
     std::vector<double> x;
