422e98f08105d3800d47970b26cf33c79050bdbb
Msmargarita1 <90843818+Msmargarita1@users.noreply.github.com>
2023-03-10 20:37:36 +0300

    Delete CTestTestfile.cmake

--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/margarita/SLAU/test
-# Build directory: /home/margarita/SLAU/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[run_test_matrix]=] "/home/margarita/SLAU/test/run_test_matrix")
-set_tests_properties([=[run_test_matrix]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/margarita/SLAU/test/CMakeLists.txt;18;add_test;/home/margarita/SLAU/test/CMakeLists.txt;0;")
